b525f36c0de30d61677dfc1d321d10c1bd3d1b8e
anakis <evansdurandx@gmail.com>
2013-03-20 23:35:32 -0400

    test 9

--- HomeSell2/src/application/FrontController.java
+++ HomeSell2/src/application/FrontController.java
@@ -19,7 +19,7 @@ public class FrontController extends HttpServlet {
 	}
 
 	public static void prepareDbRegistry(String db_id) {
-		//testing change 5
+		//testing change 9
 	}
 
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
